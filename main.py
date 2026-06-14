86be9aed060d4b6e87e73f266b550db0ba14acab
venegasb431-source <venegasb431@gmail.com>
2026-06-13 22:11:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import os
+from telegram import Update
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
+# Responder mensajes
+async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    mensaje = update.message.text.lower()
+
+    if "hola" in mensaje:
+        await update.message.reply_text(
+            "💇 ¡Hola! Soy tu asistente del salón."
+        )
+    else:
+        await update.message.reply_text(
+            f"Recibí tu mensaje: {mensaje}"
+        )
+
+# Iniciar el bot
+def main():
+    token = os.getenv("TOKEN")
+
+    app = Application.builder().token(token).build()
+
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, responder)
+    )
+
+    print("Bot iniciado...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
